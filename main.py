2e2b6e4add466b7f9ff7b442e00175c5da1ba91c
Basavarajrp <53463323+Basavarajrp@users.noreply.github.com>
2023-07-31 13:18:34 +0530

    Update main.py
    Updated code for search text.

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ if __name__ == "__main__":
     pdf_processor = PDFProcessor(chromadb_path, openai_api_key, openai_model_name)
     # pdf in the current directory
     pdf_processor.pdf_handler("sample.pdf")
-    search = pdf_processor.handleSearch("Factana")
+    search = pdf_processor.handleSearch("User search text")
     print("Search Result: ", search)
 
     
